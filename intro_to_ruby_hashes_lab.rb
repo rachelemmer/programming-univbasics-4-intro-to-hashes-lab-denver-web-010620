fea6ea36a1a42f0379127f8b4228e508ae9bdd1d
Rachel Emmer <rachelaemmer@gmail.com>
2019-12-21 21:07:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,8 +27,11 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash,key)
-  hash = {key}
-  hash[key]
+  if hash[key]
+    key += 1
+  else 
+    nil
+  end
 end
 
 def update_counting_hash(hash,key)
