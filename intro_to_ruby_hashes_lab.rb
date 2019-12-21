eabd1c93ffa366d4f04243a5419a418c804557b0
Rachel Emmer <rachelaemmer@gmail.com>
2019-12-21 21:14:37 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,14 +27,14 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash,key)
-
+  hash[key]
 end
 
 def update_counting_hash(hash,key)
   if hash[key]
   hash[key] += 1
-  end 
-else
+  else
   hash[key] = 1
   end
+hash
 end
